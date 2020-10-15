d1540f838a4748b571a7725071e67810e4696871
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-10-14 22:49:46 -0400

    add react routes to App

--- src/App.js
+++ src/App.js
@@ -6,24 +6,21 @@ import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
+import { Route, Router, Switch } from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path = "/" exact component = {() => <Home />} />
+          <Route path = "/about" exact component = {() => <About />} />
+          <Route path = "/contact" exact component = {() => <Contact />} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
